30b640c3579d59041291997c550c5f5bc6521931
marenh <72942304+marenhamby@users.noreply.github.com>
2020-12-07 22:16:19 -0500

    added role and employee tables to database

--- seed.sql
+++ seed.sql
@@ -11,4 +11,20 @@ CREATE TABLE department (
     id  INT,
     name VARCHAR(30),
     PRIMARY KEY (id)
-)
+);
+
+CREATE TABLE role (
+    id INT,
+    title VARCHAR(30),
+    salary DECIMAL(10,2),
+    department_id INT,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE employee (
+    id INT,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INT,
+    manager_id INT,
+);
