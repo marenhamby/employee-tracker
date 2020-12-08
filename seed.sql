5e3d3c7e795484a7516879b6dd941a11357419e7
marenh <72942304+marenhamby@users.noreply.github.com>
2020-12-07 22:09:21 -0500

    added pseudocode/descriptions for the first few lines of code for deleting and creating the database

--- seed.sql
+++ seed.sql
@@ -1,7 +1,10 @@
+--remove existing database, if already present
 DROP DATABASE IF EXISTS staff_trackerDB;
 
+--create new database for this project
 CREATE DATABASE staff_trackerDB;
 
+--outline which databse will be used for the rest of this document
 USE staff_trackerDB;
 
 CREATE TABLE department (
