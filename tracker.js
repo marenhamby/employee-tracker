873daa5995874004ae65831b9223442b41a64567
marenh <72942304+marenhamby@users.noreply.github.com>
2020-12-09 09:48:06 -0500

    updated function to change role with class TA assistance

--- tracker.js
+++ tracker.js
@@ -4,21 +4,21 @@ const inquirer = require("inquirer");
 
 //Outline information for database connection
 var connection = mysql.createConnection({
-  host: "localhost",
+    host: "localhost",
 
-  //Add the port for mysql
-  port: 3306,
+    //Add the port for mysql
+    port: 3306,
 
-  //Add default username
-  user: "root",
+    //Add default username
+    user: "root",
 
-  //Add default password
-  password: "",
-  database: "staff_trackerDB"
+    //Add default password
+    password: "",
+    database: "staff_trackerDB"
 });
 
 //Make the connection to the database
-connection.connect(function(err) {
+connection.connect(function (err) {
     if (err) throw err;
     //Run function to start asking the user questions
     startTracker();
@@ -30,7 +30,7 @@ connection.connect(function(err) {
 
 //first ask the user what task they would like to do
 function startTracker() {
-    inquirer.prompt({  
+    inquirer.prompt({
         type: 'list',
         message: 'What would you like to do?',
         name: 'task',
@@ -38,21 +38,21 @@ function startTracker() {
     }).then(function (answer) {
         //change what is presented to the user based on their answer to the question above
         switch (answer.task) {
-        case 'View all employees':
-            currentEmployees();
-            break;
+            case 'View all employees':
+                currentEmployees();
+                break;
 
-        case 'Add employee':
-            addEmployees();
-            break;
+            case 'Add employee':
+                addEmployees();
+                break;
 
-        case 'Change employee role':
-            changeRole();
-            break;
+            case 'Change employee role':
+                changeRole();
+                break;
 
-        case 'Exit':
-            connection.end();
-            break;   
+            case 'Exit':
+                connection.end();
+                break;
         }
     });
 };
@@ -64,52 +64,52 @@ function currentEmployees() {
     FROM ((employee 
     LEFT JOIN role ON employee.role_id = role.id) 
     LEFT JOIN department ON role.department_id = department.id)`,
-    function (err, res) {
-        if (err) throw err;
-        console.table(res);
-        startTracker();
-    });
+        function (err, res) {
+            if (err) throw err;
+            console.table(res);
+            startTracker();
+        });
 };
 
 //Add function to add an employee
 function addEmployees() {
     inquirer.prompt([
-        {  
-        type: 'input',
-        message: "What is the employee's first name?",
-        name: 'first',
+        {
+            type: 'input',
+            message: "What is the employee's first name?",
+            name: 'first',
         },
-        {  
-        type: 'input',
-        message: "What is the employee's last name?",
-        name: 'last',
+        {
+            type: 'input',
+            message: "What is the employee's last name?",
+            name: 'last',
         },
-        {  
-        type: 'list',
-        message: "What is the employee's role?",
-        name: 'role',
-        choices: ['Sales Lead', 'Salesperson', 'Lead Engineer', 'Software Engineer', 'Jr. Software Engineer', 'COO', 'Accountant', 'Legal Team Lead', 'Lawyer']
+        {
+            type: 'list',
+            message: "What is the employee's role?",
+            name: 'role',
+            choices: ['Sales Lead', 'Salesperson', 'Lead Engineer', 'Software Engineer', 'Jr. Software Engineer', 'COO', 'Accountant', 'Legal Team Lead', 'Lawyer']
         },
     ]).then(function (answer) {
         switch (answer.role) {
             case 'Sales Lead': answer.role = 1;
-            break;
+                break;
             case 'Salesperson': answer.role = 2;
-            break;
+                break;
             case 'Lead Engineer': answer.role = 3;
-            break;
+                break;
             case 'Software Engineer': answer.role = 4;
-            break;
+                break;
             case 'Jr. Software Engineer': answer.role = 5;
-            break;
+                break;
             case 'COO': answer.role = 6;
-            break;
+                break;
             case 'Accountant': answer.role = 7;
-            break;
+                break;
             case 'Legal Team Lead': answer.role = 8;
-            break;
+                break;
             case 'Lawyer': answer.role = 9;
-            break;
+                break;
         }
         //add the new user to the employee database
 
@@ -120,8 +120,8 @@ function addEmployees() {
                 last_name: answer.last,
                 role_id: answer.role
             },
-            function(err) {
-                if(err) throw err;
+            function (err) {
+                if (err) throw err;
                 console.log("added!");
                 startTracker();
             }
@@ -131,34 +131,32 @@ function addEmployees() {
 
 // Add function to change an employee's role
 function changeRole() {
-    connection.query("SELECT * FROM employee", function(err, results) {
-       if (err) throw err;
-        inquirer.prompt([
-            {  
-            type: 'list',
-            message: 'Whose role would you like to change?',
-            name: 'person',
-            choices: function () {
-                var choiceArray = [];
-                for (var i = 0; i < results.length; i++) {
-                    choiceArray.push(results[i].first_name + " " + results[i].last_name);
+    connection.query("SELECT * FROM employee", function (err, results) {
+        if (err) throw err;
+        const empList = results.map(person => { return { name: `${person.first_name} ${person.last_name}`, value: person.id } })
+        connection.query("SELECT * FROM role", function (err, results) {
+            const roleList = results.map(role => { return { name: role.title, value: role.id } })
+            inquirer.prompt([
+                {
+                    type: 'list',
+                    message: 'Whose role would you like to change?',
+                    name: 'person',
+                    choices: empList
+                },
+                {
+                    type: 'list',
+                    message: "What is the employee's new role?",
+                    name: 'role',
+                    choices: roleList
                 }
-                return choiceArray;
-            }
-            },
-            {
-            type: 'list',
-            message: "What is the employee's new role?",
-            name: 'role',
-            choices: ['Sales Lead', 'Salesperson', 'Lead Engineer', 'Software Engineer', 'Jr. Software Engineer', 'COO', 'Accountant', 'Legal Team Lead', 'Lawyer']
-            }
-        ]).then(function(answer){
-            console.log(answer);
-            var name = answer.person;
-            console.log(name);
+            ]).then(function ({person, role}) {
+                connection.query("UPDATE employee SET role_id = ? WHERE id = ?",[role, person], err=>{
+                    if (err) console.log(err)
+                    startTracker()
+                })
+            })
         })
-
     })
-    
+
 
 };
