02739753978f5d2166e0fcfbf043825435bf9dbe
marenh <72942304+marenhamby@users.noreply.github.com>
2020-12-08 20:27:27 -0500

    added quotation marks to add the names to the department table correctly

--- seed.sql
+++ seed.sql
@@ -3,9 +3,6 @@ USE staff_trackerDB;
 
 -- add starter employee info to the database --
 
-
--- create tables to be used in this application --
-
 INSERT INTO department (name)
-VALUES (Sales), (Engineering), (Finance), (Legal);
+VALUES ('Sales'), ('Engineering'), ('Finance'), ('Legal');
 
